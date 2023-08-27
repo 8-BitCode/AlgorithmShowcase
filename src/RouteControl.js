dba118031b394a6e26beaad414f0cedaa6d2ef3d
8-BitCode <76620664+8-BitCode@users.noreply.github.com>
2023-08-27 21:13:57 +0100

    Update RouteControl.js

--- src/RouteControl.js
+++ src/RouteControl.js
@@ -6,7 +6,7 @@ import PathZone from './PathZone';
 import SearchZone from './SearchZone'
 const App = () => {
   return (
-      <Router basename="/AlgorithmShowcase">
+      <Router>
         <Routes>
           <Route path="/" element={<OpenerPage />} />
           <Route path="/SortZone" element={<SortZone />} />
